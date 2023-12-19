1df3d318beef9c114e1a6c9d755238277496974f
kevjmay <kevinjohnmyburgh@icloud.com>
2023-12-19 06:57:10 +0000

    UI tweaks and linked button values to display screen through an event handler

--- index.js
+++ index.js
@@ -25,6 +25,84 @@ function operate(op) {
     return op
 }
 
+//Link the screen via a display variable
+let display = document.querySelector('.screenText')
+display.addEventListener('click', connected)
+
+function connected() {
+    display.textContent = this.textContent
+}
+
+//Links for the buttons
+let one = document.querySelector('#one')
+one.addEventListener('click', connected)
+
+let two = document.querySelector('#two')
+two.addEventListener('click', connected)
+
+let three = document.querySelector('#three')
+three.addEventListener('click', connected)
+
+let four = document.querySelector('#four')
+four.addEventListener('click', connected)
+
+let five = document.querySelector('#five')
+five.addEventListener('click', connected)
+
+let six = document.querySelector('#six')
+six.addEventListener('click', connected)
+
+let seven = document.querySelector('#seven')
+seven.addEventListener('click', connected)
+
+let eight = document.querySelector('#eight')
+eight.addEventListener('click', connected)
+
+let nine = document.querySelector('#nine')
+nine.addEventListener('click', connected)
+
+let zero = document.querySelector('#zero')
+zero.addEventListener('click', connected)
+
+let period = document.querySelector('#period')
+period.addEventListener('click', connected)
+
+let equals = document.querySelector('#equals')
+equals.addEventListener('click', connected)
+
+let subtraction = document.querySelector('#subtraction')
+subtraction.addEventListener('click', connected)
+
+let addition = document.querySelector('#addition')
+addition.addEventListener('click', connected)
+
+let multiplication = document.querySelector('#multiplication')
+multiplication.addEventListener('click', connected)
+
+let modButton = document.querySelector('#modButton')
+modButton.addEventListener('click', connected)
+
+let division  = document.querySelector('#division')
+division.addEventListener('click', connected)
+
+let deleteButton = document.querySelector('#deleteButton')
+deleteButton.addEventListener('click', deleteFunc)
+
+let clearButton = document.querySelector('#clearButton')
+clearButton.addEventListener('click', clearFunc)
+
+// Delete and clear functions
+function deleteFunc() {
+    display.textContent = '';
+    //This will need to be elaborated on when storing values in an array to pop off the last element.
+}
+
+function clearFunc() {
+    display.textContent = '';
+    //This will need to be elaborated on when storiing values in an array to empty the array.
+}
+
+
 console.log(operate(subtract(a,b)))
 
 console.log(add(a, b))
